c3b260d38b7c3a378870da38e88926fcd419dd27
Trda092 <73659917+Trda092@users.noreply.github.com>
2020-10-30 15:56:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        p {text-align: center;}
-    </style>
-
-</head>
-<body>
-    <p>
-        <center>
-            <img src="bigbrain.jpg">
-        <img src="superbrain.jpg">
-        </center>
-    </p>
-    <p>
-        
-    </p>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Lab_ITF ; Tada Ampornprasert</title>
+        <p>Name ; Tada Ampornprasert</p>
+        <p>Nickname ; Ohm</p>
+    <body>
+        <style>
+            p {text-align: center;}
+            .btn {
+              background-color: #2196F3;
+              color: white;
+              padding: 16px;
+              font-size: 16px;
+              border: none;
+              outline: none;
+            }
+            
+            .dropdown {
+              position: absolute;
+              display: inline-block;
+            }
+            
+            .dropdown-content {
+              display: none;
+              position: absolute;
+              background-color: #f1f1f1;
+              min-width: 160px;
+              z-index: 1;
+            }
+            
+            .dropdown-content a {
+              color: black;
+              padding: 12px 16px;
+              text-decoration: none;
+              display: block;
+            }
+            
+            .dropdown-content a:hover {background-color: #ddd}
+            
+            .dropdown:hover .dropdown-content {
+              display: block;
+            }
+            
+            .btn:hover, .dropdown:hover .btn {
+              background-color: #0b7dda;
+            }
+            </style>
+            </head>
+            <body><center>
+            <button class="btn">Button</button>
+            
+            <div class="dropdown">
+              <button class="btn" style="border-left:1px solid #0d8bf2">
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="https://ejudge.it.kmitl.ac.th/" style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Click this link : If you want to burn your brain</a>
+                <a href="page2.html" style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Jocking</a>
+              </div>
+            
+            </div></center>
 </body>
 </html>
